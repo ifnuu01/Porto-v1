949e58ae5eb5b5cb834e2ec65c435f5b54db6af9
ifnuu01 <ifnuu01@gmail.com>
2025-02-21 21:47:45 +0800

    feat : menambahkan section layout 1 home.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Home from "./layouts/home";
+
+function App() {
+  return (
+    <div className="bg-ifnu h-[1000px]">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
