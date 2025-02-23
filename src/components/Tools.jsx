5351424adf190408b4287e222ab1302fd4ea355b
ifnuu01 <ifnuu01@gmail.com>
2025-02-23 13:21:19 +0800

    feat : menambahkan tools component

---
+++ src/components/Tools.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import Marquee from "react-fast-marquee";
+
+export default function Tools() {
+  const tools = [
+    {
+      id: 1,
+      name: "HTML",
+    },
+    {
+      id: 2,
+      name: "CSS",
+    },
+    {
+      id: 3,
+      name: "JavaScript",
+    },
+    {
+      id: 4,
+      name: "PHP",
+    },
+    {
+      id: 5,
+      name: "Python",
+    },
+    {
+      id: 6,
+      name: "ReactJs",
+    },
+    {
+      id: 7,
+      name: "Networking",
+    },
+    {
+      id: 8,
+      name: "Mikrotik",
+    },
+    {
+      id: 9,
+      name: "Bootstrap",
+    },
+    {
+      id: 10,
+      name: "TailwindCSS",
+    },
+    {
+      id: 11,
+      name: "Laravel",
+    },
+    {
+      id: 12,
+      name: "NodeJs",
+    },
+  ];
+  return (
+    <div className="mt-4 w-96 lg:w-1/2">
+      <h1 className="text-white font-bold text-2xl mt-4 font-[Roboto] text-center">
+        ⚒️ Language & Tools ⚒️
+      </h1>
+      <Marquee
+        gradient={false}
+        speed={40}
+        autoFill
+        className="mt-8 lg:max-w-fit"
+      >
+        {/* Button Tools */}
+        {tools.map((tool) => (
+          <div
+            key={tool.id}
+            className="bg-[#141414] text-white font-semibold p-2 rounded-lg cursor-pointer hover:bg-[#1f1f1f] transition duration-30 mr-2"
+          >
+            {tool.name}
+          </div>
+        ))}
+      </Marquee>
+    </div>
+  );
+}
