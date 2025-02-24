bc4a56a5a316f463d7fa22e524c8fe502ae83e78
ifnuu01 <ifnuu01@gmail.com>
2025-02-24 18:12:31 +0800

    fixed : bug path

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import About from "./components/About";
 import Home from "./components/Home";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import Project from "./components/Project";
 
 function App() {
